4857ef3e27530d9f2564539238b58d5b38e5b5aa
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-01 11:40:09 +0330

    show search input result

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,34 @@
 import { getAllCities } from "../../utils/shared.js";
 
 const loadingContainer = document.querySelector('#loading-container')
+
 window.addEventListener("load", () => {
   loadingContainer.style.display ="none"
   getAllCities().then((response) => {
     const popularCitiesContainer = document.querySelector("#popular-cities");
     const popularCities = response.data.cities.filter((city) => city.popular);
+
+    const searchInput = document.querySelector('#search-input')
+    const serachResult = document.querySelector('.search-result-cities')
+    searchInput.addEventListener('keyup',(event)=>{
+     if(event.target.value.length){
+      serachResult.classList.add('active')
+      const searchResultCities = response.data.cities.filter(city=> city.name.startsWith(event.target.value))
+      console.log(searchResultCities);
+       if(searchResultCities.length){
+        serachResult.innerHTML=''
+        searchResultCities.forEach(city => {
+          serachResult.insertAdjacentHTML('beforeend',`
+            <li>${city.name}</li>
+            `)
+        })
+       }
+
+     }else{
+      serachResult.classList.remove('active')
+     }
+
+    })
 
     popularCities.forEach((city) => {
       popularCitiesContainer.insertAdjacentHTML(
--- utils/shared.js
+++ utils/shared.js
@@ -11,12 +11,12 @@ const getAndShowSocials = async ()=> {
     const socialMediaContainer = document.querySelector('#footer__social-media')
     const res = await fetch(`${baseUrl}/v1/social`)
     const socialsResponse = await res.json()
-    console.log(socialsResponse,res);
+    
     socialsResponse.data.socials.forEach(social => {
         console.log(socialsResponse.data.socials);
         socialMediaContainer.insertAdjacentHTML('beforeend',`
     <a href="" class="sidebar__icon-link">
-     <img src="${social.icon}" alt="${social.name}" href="${social.link}" width="18px" height="18px" class="sidebar__icon bi bi-twitter" />
+     <img  width="18px" height="18px" alt="${social.name}" href="${social.link}" src="${social.icon}"  class="sidebar__icon bi bi-twitter" />
     </a>
     `)
     });

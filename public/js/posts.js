b9ec7545ee05be2991c85ac412a2b5657235ce21
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-06 20:31:49 +0330

    biulding generatePOsts function to see post in each city

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,5 +1,6 @@
 import { getPosts } from "../../utils/shared.js"
 import { getFromLocalStorage } from "../../utils/utils.js"
+import { baseUrl } from "../../utils/shared.js"
 
 
 
@@ -12,5 +13,57 @@ console.log('log');
         loadingContainer.style.display='none'
         console.log(response);
         const posts =response.data.posts
+
+        generatePosts(posts)
     })
+
+    const generatePosts = (posts)=>{
+       const postContainer = document.querySelector('#posts-container')
+       if(posts.length){
+        posts.forEach(post => {
+            postContainer.insertAdjacentHTML('beforeend',`
+                <div class="col-4">
+              <a href="post.html/id=${post._id}" class="product-card">
+                <div class="product-card__right">
+                  <div class="product-card__right-top">
+                    <p class="product-card__link">${post.title}</p>
+                  </div>
+                  <div class="product-card__right-bottom">
+                    <span class="product-card__condition">${
+                      post.dynamicFields[0].data
+                    }</span>
+                    <span class="product-card__price">
+                      ${
+                        post.price === 0
+                          ? "توافقی"
+                          : post.price.toLocaleString() + " تومان"
+                      }
+                    </span>
+                    <span class="product-card__time">Date</span>
+                  </div>
+                </div>
+                <div class="product-card__left">
+                ${
+                  post.pics.length
+                    ? `
+                      <img
+                        class="product-card__img img-fluid"
+                        src="${baseUrl}/${post.pics[0].path}"
+                      />`
+                    : `
+                      <img
+                        class="product-card__img img-fluid"
+                        src="/public/images/main/noPicture.PNG"
+                      />`
+                }
+                  
+                </div>
+              </a>
+            </div>
+                `)
+        });
+       }else{
+        postContainer.innerHTML = '<p class="empty">اگهی یافت نشد</p>'
+       }
+    }
 })
